97052f24e9460ce27bac0bc245ca7414ecf5870f
Daodao Zhou <dd19@hemlock.rlogin>
2024-03-13 01:24:26 -0400

    update program

--- Generic/simulation.c
+++ Generic/simulation.c
@@ -1,3 +1,7 @@
+/*
+main simulation part handle everything
+Usage: ./program <input_filename> [NUMofRUNS] [SimulationTime]
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -5,29 +9,39 @@
 #include "model.h"
 
 /*
-These value should be input in command prompy by user
+Defult value 
 */
-//#define BINNUM 100
-#define NUMofRUNS 1 
-#define SimulationTime 10
+int NUMofRUNS = 1; 
+int SimulationTime = 10; 
 
 double diffusionRates[N] = DIFFUSION_RATE;
 int reactionTypes[M] = REACTION_TYPE;
 double rateConstants[M] = RATE_CONSTANTS;
-int sepciesRatio[N] = SPECIES_RATIO;
-int BINNUM =100; //this value should also input in command prompy by user
+
+
 int main(int argc, char *argv[])
 {
-    int pm = BINNUM / BINNUM;
-    
-    double w = 100; //scale factor, it is a fixed number?
+    char* inputFilename;
+    if (argc > 1) {
+        inputFilename = argv[1];
+        if (argc > 2) {
+            NUMofRUNS = atoi(argv[2]);
+        }
+        if (argc > 3) {
+            SimulationTime = atoi(argv[3]);
+        }
+    } else {
+        fprintf(stderr, "Usage: %s <input_filename> [NUMofRUNS] [SimulationTime]\n", argv[0]);
+        return 1; 
+    }
+ 
     /*
     calculate h
     */
     double h[N];
     for (int i = 0; i < N; i++)
     {
-        h[i] = (double)L / BINNUM;
+        h[i] = L / W;
     }
  
     /*
@@ -39,71 +53,71 @@ int main(int argc, char *argv[])
         jumpRate[i] = diffusionRates[i] / (h[i] * h[i]);
     }
    
-    const int RULENUM = 2 * N + M;
+    
+    const int RULENUM = N + M;
     double a[RULENUM], a0;
     double r1, r2, tau;
     int ita;
     double sum, sum_a, sum_x;
 
-    /*
-    for output
-    */
-    long firings[RULENUM], location[BINNUM];
+
+    long firings[RULENUM], location[W];
     FILE *population = fopen("population", "w");
     FILE *total = fopen("total", "w");
     FILE *firefile = fopen("firings", "w");
     FILE *locationfile = fopen("location", "w"); 
 
     printf("Begin the model simulation ...\n");
-    printf("XBINNUM = %d, YBINNUM = %d\n", BINNUM, BINNUM);
+    printf("BINNUM = %d\n", W);
 
+    srand(time(NULL)); 
     for (int real = 0; real < NUMofRUNS; real++)
     {
-        
-        srand(time(NULL));
         /*
         set total population for each species
         */
         long tot[N];
         for (int n = 0; n < N; n++)
         {
-            tot[n] = sepciesRatio[n] * w;
+            tot[n] =  S;
         }
 
         /*
-        set population value for each species in each bin
+        set population value for each species in each bin from the input file
         */
-        long **space;
-        space = (long **)malloc(N * sizeof(long *));
+        FILE *inputfile;
+        inputfile = fopen(inputFilename, "r");
+
+        long **species_population;
+        species_population = (long **)malloc(N * sizeof(long *));
         for (int n = 0; n < N; n++)
         {    
-            space[n] = (long *)malloc(BINNUM * sizeof(long));
-            for (int m = 0; m < BINNUM; m++)
-            {
-                space[n][m] = sepciesRatio[n] * w / BINNUM;
-            }
+            species_population[n] = (long *)malloc(W * sizeof(long));
         }
-
-        //printf("space is %ld",space[0][0]);
+        
+        for (int n = 0; n < N; n++) {
+        for (int m = 0; m < W; m++) {
+            fscanf(inputfile, "%ld", &species_population[n][m]);
+        }
+    }
         /*
         for writing output
         */
         for (int i = 0; i < RULENUM; i++)
             firings[i] = 0;
-        for (int i = 0; i < BINNUM; i++)
+        for (int i = 0; i < W; i++)
             location[i] = 0;
 
         double timeTracker = 0.0; 
-        //printf("a0 is %f and %f \n", jumpRate[0],(tot[0] - space[0][0]));
         while (timeTracker < SimulationTime)
         {
             /*
             calculate propensity for diffusion for each species
             */
-            for (int n = 0; n < N; n++)
+              for (int n = 0; n < N; n++)
             {
-                a[2 * n] = jumpRate[n] * (tot[n] - space[n][0]);
-                a[2 * n + 1] = jumpRate[n] * (tot[n] - space[n][BINNUM - 1]);
+                a[n] = jumpRate[n] * (tot[n]);
+            
             }
             /*
             calculate propneisty for each reaction channel
@@ -112,42 +126,22 @@ int main(int argc, char *argv[])
             {
                 if (reactionTypes[run] == 1)
                 {
-                    a[2 * N + run] = rateConstants[run] * w;
+                    a[N + run] = rateConstants[run] * S;
 
                 }
                 else if (reactionTypes[run] == 2)
                 {
-                    a[2 * N + run] = rateConstants[run] * tot[0];
+                    a[N + run] = rateConstants[run] * tot[0];
                    
                 }
                 else if (reactionTypes[run] == 3)
                 {
-                    ;
-                }
-                else if (reactionTypes[run] == 4)
-                {
-                    a[2 * N + run] = 0;
-                    for (int i = 0; i < BINNUM; i++)
-                    {          
-                        long temp = space[N - 2][i] * (space[N - 2][i] - 1) * space[N-1][i / pm];
-                        //printf("%ld and %ld and %ld\n",space[N - 2][i],(space[N - 2][i] - 1),space[N-1][i / pm]);
-                        a[2 * N + run] += temp; 
-                       
-                    }
+
+                    a[N + run]=prop3(W, species_population);
                    
-                     a[2 * N + run] *= rateConstants[run] * BINNUM * BINNUM;
                 }
     
             }
-            // printf("a0 is %f\n",a[0]);
-            // printf("a1 is %f\n",a[1]);
-            // printf("a2 is %f\n",a[2]);
-            // printf("a3 is %f\n",a[3]);
-            // printf("a4 is %f\n",a[4]);
-            // printf("a5 is %f\n",a[5]);
-            // printf("a6 is %f\n",a[6]);
-            // printf("a7 is %f\n",a[7]);
-
             /*
             sum total propensity
             */
@@ -178,86 +172,99 @@ int main(int argc, char *argv[])
             sum_a -= a[ita];
             firings[ita]++;
 
-            int i;
-            if (ita < 2 * N)
+            int i;//index of bin
+            if (ita < N)
             {    
                 if (ita % 2 == 0) //even number is the species diffuse to left
                 {
                     r2 = (sum - sum_a) / a[ita];
-                    sum = (tot[ita / 2] - space[(ita / 2)][0]) * r2;
-                    sum_x = space[(ita / 2)][1];
+                    sum = (tot[ita / 2] - species_population[(ita / 2)][0]) * r2;
+                    sum_x = species_population[(ita / 2)][1];
                     i = 1;
                     while (sum_x < sum)
                     {
                         i++;
-                        sum_x += space[(ita / 2)][i];
+                        sum_x += species_population[(ita / 2)][i];
                     }
-                    space[(ita / 2)][i]--;
-                    space[(ita / 2)][i - 1]++;
-                    location[i]++;
+                    species_population[(ita / 2)][i]--;
+                    species_population[(ita / 2)][i - 1]++;
+                    location[i]++;  
                   
                 }
 
                 else// odd number is species diffuse to right
                 { 
+                    r2 = (sum - sum_a) / a[ita];
+                    sum = (tot[ita / 2+1] - species_population[(ita / 2+1)][0]) * r2;
+                    sum_x = species_population[(ita / 2+1)][1];
+                    i = 1;
+                    while (sum_x < sum)
+                    {
+                        i++;
+                        sum_x += species_population[(ita / 2+1)][i];
+                    }
+                    species_population[(ita / 2+1)][i]--;
+                    species_population[(ita / 2+1)][i - 1]++;
+                    location[i]++;
+
                     i = 0;
                     r2 = (sum - sum_a) / a[ita];
-                    sum = (tot[ita / 2] - space[ita / 2][BINNUM - 1]) * r2;
-                    sum_x = space[ita / 2][0];
+                    sum = (tot[ita / 2+1] - species_population[ita / 2+1][W - 1]) * r2;
+                    sum_x = species_population[ita / 2+1][0];
 
                     while (sum_x < sum)
                     {
                         i++;
-                        sum_x += space[ita / 2][i];
+                        sum_x += species_population[ita / 2+1][i];
                     }
-                    space[ita / 2][i]--;
-                    space[ita / 2][i + 1]++;
+                    species_population[ita / 2+1][i]--;
+                    species_population[ita / 2+1][i + 1]++;
                     location[i]++;
                 
                 }
             }
             else
             {
-                if (reactionTypes[ita - 2 * N] == 1)
+                if (reactionTypes[ita - N] == 1)
                 { 
                     r2 = (sum - sum_a) / a[ita];
-                    i = r2 * BINNUM; // Find the index of the bin for the new X to appear at
-                    space[(ita - 2 * N) / 2][i]++;
-                    tot[(ita - 2 * N) / 2]++;
+                    i = r2 * W; 
+                    species_population[(ita - N) / 2][i]++;
+                    tot[(ita - N) / 2]++;
                     location[i]++;
                   
                 }
-                if (reactionTypes[ita - 2 * N] == 2)
+                if (reactionTypes[ita - N] == 2)
                 { 
                     r2 = (sum - sum_a) / a[ita];
-                    sum_x = space[(ita - 2 * N) / 2][0];
-                    sum = r2 * tot[(ita - 2 * N) / 2];
+                    sum_x = species_population[(ita - N) / 2][0];
+                    sum = r2 * tot[(ita - N) / 2];
                     i = 0;
                     while (sum_x < sum)
                     {
                         i++;
-                        sum_x += space[(ita - 2 * N) / 2][i];
+                        sum_x += species_population[(ita -  N) / 2][i];
                     }
 
-                    space[(ita - 2 * N) / 2][i]--;
-                    tot[(ita - 2 * N) / 2]--;
+                    species_population[(ita -  N) / 2][i]--;
+                    tot[(ita -N) / 2]--;
                     location[i]++;
                    
-                }//rule type 3 omit for now
-                if (reactionTypes[ita - 2 * N] == 4)
+                }
+                if (reactionTypes[ita - N] == 3)
                 {
-                    sum = (sum - sum_a) / (rateConstants[ita - 2 * N] * BINNUM * BINNUM);
-                    sum_x = space[(ita - 2 * N) / 2 - 1][0] * (space[(ita - 2 * N) / 2 - 1][0] - 1) * space[(ita - 2 * N) / 2][0];
+                    sum = (sum - sum_a) / (rateConstants[ita -  N] * W * W);
+                    sum_x = species_population[(ita - N) / 2 - 1][0] * (species_population[(ita - N) / 2 - 1][0] - 1) * species_population[(ita - N) / 2][0];
                     i = 0;
                     while (sum_x < sum)
                     {
                         i++;
-                        sum_x += space[(ita - 2 * N) / 2 - 1][i] * (space[(ita - 2 * N) / 2 - 1][i] - 1) * space[(ita - 2 * N) / 2][i / pm];
+                        sum_x += species_population[(ita -  N) / 2 - 1][i] * (species_population[(ita - N) / 2 - 1][i] - 1) * species_population[(ita - N) / 2][i];
                     }
-                    space[(ita - 2 * N) / 2 - 1][i]++;
-                    tot[(ita - 2 * N) / 2 - 1]++;
-                    space[(ita - 2 * N) / 2][i / pm]--;
-                    tot[(ita - 2 * N) / 2]--;
+                    species_population[(ita -  N) / 2 - 1][i]++;
+                    tot[(ita - N) / 2 - 1]++;
+                    species_population[(ita - N) / 2][i]--;
+                    tot[(ita - N) / 2]--;
                     location[i]++;
                   
                 }
@@ -266,8 +273,8 @@ int main(int argc, char *argv[])
 
         for (int i = 0; i < N; i++)
         {
-            if (i = N - 1)
-            { // the last species
+            if (i == N - 1)
+            { 
                 fprintf(total, "%ld\n", tot[i]);
             }
             else
@@ -278,24 +285,26 @@ int main(int argc, char *argv[])
 
         for (int i = 0; i < N; i++)
         {
-            for (int j = 0; j < BINNUM; j++)
+            for (int j = 0; j < W; j++)
             {
-                fprintf(population, "%ld\t", space[i][j]);
+                fprintf(population, "%ld\t", species_population[i][j]);
             }
+            fprintf(population, "\n");
         }
-        fprintf(population, "\n");
+      
 
         for (int i = 0; i < RULENUM; i++)
             fprintf(firefile, "%ld\t", firings[i]);
         fprintf(firefile, "\n");
-        for (int i = 0; i < BINNUM; i++)
+        for (int i = 0; i < W; i++)
             fprintf(locationfile, "%ld\t", location[i]);
         fprintf(locationfile, "\n");
 
          for (int n = 0; n < N; n++) {
-        free(space[n]); 
+        free(species_population[n]); 
     }
-    free(space); 
+    free(species_population); 
+    fclose(inputfile);
     }
 
     printf("End of simulation ...\n");
@@ -308,3 +317,4 @@ int main(int argc, char *argv[])
    
     return 0;
 }
+
