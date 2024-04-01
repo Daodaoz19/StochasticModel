ab03346447782c1286e7def1b46e900bc960f5a5
Daodao Zhou <dd19@perisimmon.rlogin>
2024-03-31 21:07:57 -0400

    update modeler file

--- Generic/model.c
+++ Generic/model.c
@@ -1,27 +1,72 @@
-/*
-model.c file provied by user
-*/
-#include "model.h" 
-#include <stdlib.h>
-#include <stdio.h>
+#include "model.h"
+#include <string.h>
 
 
-void initial(long ***species_population) {
-    for (int j = 0; j < W; j++) {
-        (*species_population)[0][j] = 2; 
-        (*species_population)[1][j] = 1; 
+struct Species* createSpeciesArray() {
+    struct Species* species  = (struct Species*)malloc(n_species * sizeof(struct Species));
+    for (size_t i = 0; i < n_species; ++i) {
+        species[i].index = i; //index
+        strcpy(species[i].name, "NAME"); // Default type
+        species[i].initialValue = 0; // Default type
+        species[i].diffusionRate = 0; // Default type
     }
+    return species;
+}
+
+struct Reaction* createReactionArray() {
+    struct Reaction* reaction= (struct Reaction*)malloc(m_reaction * sizeof(struct Reaction));
+    for (size_t i = 0; i < m_reaction; ++i) {
+        reaction[i].type = 1; // Default type
+        reaction[i].rateConstant = 0; // Default type
+        reaction[i].reactant = NULL;// Default type
+        reaction[i].calculatePropensity =NULL;// Default type
+    }
+    return reaction;
+}
+
+void initializeSimulation(struct Species** species, struct Reaction** reactions) {
+    *species = createSpeciesArray(n_species);
+    *reactions = createReactionArray(m_reaction);
+    
+    strcpy((*species)[0].name, "X");
+    (*species)[0].initialValue = 2;
+    (*species)[0].diffusionRate = 5e-4;
+
+    strcpy((*species)[1].name, "Y");
+    (*species)[1].initialValue = 1;
+    (*species)[1].diffusionRate = (5e-4 * 10000);
+
+    // A -> X
+    (*reactions)[0].type = 0;
+    (*reactions)[0].rateConstant = 1.0;
+
+    
+    // X -> C
+    (*reactions)[1].type = 1;
+    (*reactions)[1].rateConstant = 2.0;
+    (*reactions)[1].reactant = (int*)malloc((*reactions)[1].type * sizeof(int));
+    (*reactions)[1].reactant[0]= 0;
+
+    //B -> Y
+    (*reactions)[2].type = 0;
+    (*reactions)[2].rateConstant = 3.0;
+
+
+    //2X + Y -> 3X
+    (*reactions)[3].type = 2;
+    (*reactions)[3].rateConstant = 1.0/w_bin*w_bin;
+    (*reactions)[3].calculatePropensity = prop3;
+   
 }
 
 void prop3(double **a,long **species_population,int rulenum) {  
-    for (int i = 0; i < W; i++) {
+    for (int i = 0; i < w_bin; i++) {
         long x = species_population[0][i]; 
         long y = species_population[1][i];
         a[rulenum][i] = x * (x - 1) * y; 
        } 
 }
 
-//reaction i will hapen in j bin
 void stateChange(int i,int j, long **species_population,long *tot) {
    switch(i) {
     case 1:
@@ -47,25 +92,3 @@ void stateChange(int i,int j, long **species_population,long *tot) {
         break;
     }
 }
-
-
-//PropensityFunc propensityFuncs[M] = {NULL, NULL, NULL, ,
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Generic/model.h
+++ Generic/model.h
@@ -1,28 +1,30 @@
-/*
-model.h file provied by user
-*/
 #ifndef MODEL_H
 #define MODEL_H
+#include <stdlib.h> 
 
-#define N 2 
-#define M 4 
-#define W 100 
+typedef void (*PropensityFunc)(double **, long **, int);
+#define n_species 2 
+#define m_reaction 4 
+#define w_bin 100 
 #define L 10.0
-#define S 100 
 
-extern double diffusionRates[N];
-extern int reactionTypes[M];
-extern double rateConstants[M];
+struct Species{
+    int index;
+    char name[100];
+    int initialValue;
+    double diffusionRate;
+};
 
-#define DIFFUSION_RATE {5e-4,(5e-4 * 10000)}
-#define REACTION_TYPE {1, 2, 1, 3}
-#define specesTot {0}
-#define RATE_CONSTANTS {1, 2, 3, 1.0/W*W}
+struct Reaction{
+    int type; 
+    double rateConstant;
+    int* reactant; 
+    PropensityFunc calculatePropensity;
+};
 
-void initial(long ***species_population);
+
+struct Species* createSpeciesArray();
+struct Reaction* createReactionArray();
 void prop3(double **a,long **species_population,int rulenum);
 void stateChange(int i,int j, long **species_population,long *tot);
-
-//extern PropensityFunc propensityFuncs[M];
-
 #endif
